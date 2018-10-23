357a7880f49650e7ea3b69c21302c703c67d1db4
asanorintaro <asanorintaro19880830@gmail.com>
2018-10-23 13:30:28 +0900

    とり急ぎコード

---
+++ clean_copy.rb
@@ -0,0 +1,94 @@
+class Vendingmachine
+  AVAILABLE_MONEY = [10, 50, 100, 500, 1000]
+  
+  def initialize
+    @amount = 0
+    @sales = 0
+    @coke_stock = 5
+    @red_bull_stock = 5
+    @water_stock = 5
+  end
+ 
+  #お金の投入
+  def insert
+    puts '金額を打ち込んで下さい(10,50,100,500,1000円のみ利用可です)'
+    money = gets.to_i
+    if AVAILABLE_MONEY.include?(money)
+      @amount += money
+      puts "#{@amount}円投入されています"
+    else
+      puts 'このお金は利用できません(10,50,100,500,1000円のみ利用可です)'
+    end  
+  end
+
+　#お金の返却
+  def refund
+    puts "#{@amount}円返却します"
+    @amount = 0
+  end
+
+  #商品の購入（在庫と売り上げの管理）
+  def purchase
+    puts '商品を選択して下さい'
+    puts '1:コーラ(120円)'
+    puts '2:レッドブル(200円)'
+    puts '3:水(100円)'
+    drink_number = gets.to_i
+
+    if drink_number == 1
+      if @coke_stock ==0
+        puts '売り切れ'
+      else
+        price = 120
+        if @amount >= price
+           @sales = price + @sales
+           @amount = @amount - price
+           @coke_stock = @coke_stock -1
+           puts 'コーラを購入しました'
+        else
+          puts 'お金が足りません'
+        end
+      end
+    elsif drink_number == 2
+      if @red_bull_stock == 0
+         puts '品切れです'
+      else
+        price = 200
+        if @amount >= price
+           @sales = price + @sales
+           @amount = @amount - price
+           @red_bull_stock = @red_bull_stock -1
+           puts 'レッドブルを購入しました'
+        else
+           puts 'お金が足りません'
+        end
+      end
+
+    else drink_number == 3
+      if @water_stock == 0
+         puts '品切れです'
+      else
+        price =200
+        if @amount >= price
+           @sales = price + @sales
+           @amount = @amount - price
+           @water_stock = @water_stock -1
+           puts '水を購入しました'
+        else
+           puts 'お金が足りません'
+        end
+      end
+    end
+  end
+
+  #在庫情報
+  def stock
+    puts "在庫情報:コーラ #{@coke_stock}個,レッドブル #{@red_bull_stock}個,水 #{@water_stock}個"
+  end
+
+  #売り上げ情報
+  def sales
+    puts "売り上げ:#{@sales}円"
+  end
+end
+# require '/Users/rintaro/ruby-training/clean-copy'
